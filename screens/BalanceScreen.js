96b27452e4d9e95e9940dd47ae285b640ed6012e
Sai Kovour <70875830+SuperSaigon123@users.noreply.github.com>
2023-05-12 10:43:34 -0500

    5/12

--- screens/BalanceScreen.js
+++ screens/BalanceScreen.js
@@ -1,15 +1,36 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, TextInput} from 'react-native';
-
+import { Button } from 'react-native-paper';
+import {InputSpinner} from 'react-native-input-spinner'
 
 export default function BalanceScreen() {
+  const [balance, setBalance] = React.useState(0);
+
   return (
     <View style={styles.container}>
-      <Text
-        onPress={() => alert('This is the "Balance" screen.')}
-        style={{ fontSize: 26, fontWeight: 'bold' }}>
-        Balance Screen
-      </Text>
+      <Text style={{color: 'white', fontSize: 32, marginBottom: 10}}>BALANCE: </Text>
+      <Text style={{color: 'grey', fontSize: 24}}>Recently spent:</Text>
+      
+      <Text>How much SC would you like to purchase?</Text>
+      <InputSpinner
+        max={10}
+        min={2}
+        step={2}
+        colorMax={"#f04048"}
+        colorMin={"#40c5f4"}
+        onChange={(balance) => {
+          console.log(balance);
+        }}
+      />;
+      <Button>BUY</Button>
+
+      <Text>How much SC would you like to withdraw?</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='0'
+      ></TextInput>
+      <Button>SELL</Button>
+      
     </View>
   );
 }
@@ -20,6 +41,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#2B2B2B',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white'
   },
 });
 
